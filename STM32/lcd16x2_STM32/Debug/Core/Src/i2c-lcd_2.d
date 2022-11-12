de290243e52ec95975f356cb5a6bfbbe854b927b
Hưng Lê <bincholon02@gmail.com>
2022-11-12 12:45:50 +0700

    update BOM + PCB minor change
    thay đổi các lỗi anh Hải sửa trong drive :D

--- STM32/lcd16x2_STM32/Debug/Core/Src/i2c-lcd_2.d
+++ STM32/lcd16x2_STM32/Debug/Core/Src/i2c-lcd_2.d
@@ -1,4 +1,4 @@
-Core/Src/i2c-lcd.o: ../Core/Src/i2c-lcd.c ../Core/Inc/i2c-lcd.h \
+Core/Src/i2c-lcd_2.o: ../Core/Src/i2c-lcd_2.c ../Core/Inc/i2c-lcd_2.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -24,29 +24,55 @@ Core/Src/i2c-lcd.o: ../Core/Src/i2c-lcd.c ../Core/Inc/i2c-lcd.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
-../Core/Inc/i2c-lcd.h:
+
+../Core/Inc/i2c-lcd_2.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
 ../Core/Inc/stm32f1xx_hal_conf.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
 ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
 ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103x6.h:
+
 ../Drivers/CMSIS/Include/core_cm3.h:
+
 ../Drivers/CMSIS/Include/cmsis_version.h:
+
 ../Drivers/CMSIS/Include/cmsis_compiler.h:
+
 ../Drivers/CMSIS/Include/cmsis_gcc.h:
+
 ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
--- STM32/lcd16x2_STM32/Debug/Core/Src/subdir.mk
+++ STM32/lcd16x2_STM32/Debug/Core/Src/subdir.mk
@@ -1,12 +1,12 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Core/Src/gpio.c \
-../Core/Src/i2c-lcd.c \
+../Core/Src/i2c-lcd_2.c \
 ../Core/Src/i2c.c \
 ../Core/Src/main.c \
 ../Core/Src/software_timer.c \
@@ -19,7 +19,7 @@ C_SRCS += \
 
 OBJS += \
 ./Core/Src/gpio.o \
-./Core/Src/i2c-lcd.o \
+./Core/Src/i2c-lcd_2.o \
 ./Core/Src/i2c.o \
 ./Core/Src/main.o \
 ./Core/Src/software_timer.o \
@@ -32,7 +32,7 @@ OBJS += \
 
 C_DEPS += \
 ./Core/Src/gpio.d \
-./Core/Src/i2c-lcd.d \
+./Core/Src/i2c-lcd_2.d \
 ./Core/Src/i2c.d \
 ./Core/Src/main.d \
 ./Core/Src/software_timer.d \
@@ -45,13 +45,6 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103x6 -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
-
-clean: clean-Core-2f-Src
-
-clean-Core-2f-Src:
-	-$(RM) ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/i2c-lcd.d ./Core/Src/i2c-lcd.o ./Core/Src/i2c-lcd.su ./Core/Src/i2c.d ./Core/Src/i2c.o ./Core/Src/i2c.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/software_timer.d ./Core/Src/software_timer.o ./Core/Src/software_timer.su ./Core/Src/stm32f1xx_hal_msp.d ./Core/Src/stm32f1xx_hal_msp.o ./Core/Src/stm32f1xx_hal_msp.su ./Core/Src/stm32f1xx_it.d ./Core/Src/stm32f1xx_it.o ./Core/Src/stm32f1xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f1xx.d ./Core/Src/system_stm32f1xx.o ./Core/Src/system_stm32f1xx.su ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su
-
-.PHONY: clean-Core-2f-Src
 
